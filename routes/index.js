ea9344f343907510240bb78f1f8beb8bc46f1aea
Thomas Slade <me@thomas-slade.com>
2017-02-19 16:58:11 -0500

    Fetch orders

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+    var Robinhood = require('robinhood')(req.app.locals.credentials, function(){
+      Robinhood.orders(function(err, response, body) {
+          if (err) {
+              console.error(err);
+          } else {
+              console.log(body);
+          }
+      });
+    });
+
   res.render('index', { title: 'Express' });
 });
 
